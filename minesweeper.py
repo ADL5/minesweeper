fb8f74875eebe65b749908280b2583f8b21318e1
ADL5 <145772698+ADL5@users.noreply.github.com>
2024-04-05 21:07:26 +0300

    Update minesweeper.py
    fixed some bugs

--- minesweeper.py
+++ minesweeper.py
@@ -8,25 +8,9 @@ from time import sleep
 import os
 import threading
 from json import load, dump
-
-data = {"record_8*8": 10000, 'record_16*16': 10000, "record_16*30": 10000,"recursion_akkord": False}
-
-if not (os.path.exists('data_minesweeper.json')):
-	with open('data_minesweeper.json', 'w') as file:
-		dump(data, file)
-else:
-	with open('data_minesweeper.json', 'r') as file:
-		data = load(file)
-
-if os.path.exists('pictures'):
-	os.chdir('pictures')
-else:
-	showerror('FileError', 'Required files were not found.')
-	exit()
-
-n = 8 #sizes field 8 8   16 16   16 30
-m = 8
-
+#сделать статистику: победы, всего игры процент побед, сколько влажков поставил, сколько клеток открыл, сколько раз в какой режим сыграл
+n = int() #sizes field 8 8   16 16   16 30
+m = int()
 k = int()
 flag_x = int()
 wid = int()
@@ -43,8 +27,25 @@ width_pic = int()
 rel_wid = int()
 hei_plus = int()
 rel_y = int()
+record8 = int()
+record16 = int()
+record30 = int()
+rel_wid = 1
+hei_plus = 50
+sizes = ['8x8','16x16','16x30']
+data = {"record_8*8": 10000, 'record_16*16': 10000, "record_16*30": 10000,"recursion_akkord": False,"total_runs":0,"winning_rund": 0}
+
+if not (os.path.exists('data_minesweeper.json')):
+	with open('data_minesweeper.json', 'w') as file:
+		dump(data, file)
+else:
+	with open('data_minesweeper.json', 'r') as file:
+		data = load(file)
+
 def th_16():
-	global k,flag_x,wid,hei,wid_plus,rel_hei_coils,flag_relx,flag_rely,btn_pady,timer_relx,size,width,width_pic,rel_wid,hei_plus,rel_y
+	global n,m,k,flag_x,wid,hei,wid_plus,rel_hei_coils,flag_relx,flag_rely,btn_pady,timer_relx,size,rel_wid,hei_plus,rel_y
+	n = 16
+	m = 30
 	k = 99
 	flag_x = 30
 	wid = 1000
@@ -56,71 +57,215 @@ def th_16():
 	btn_pady = 30
 	timer_relx = 0.85
 	size = 35
-	width = 63
-	width_pic = 63
 	rel_wid = 1
 	hei_plus = 50
-	rel_y = 125 / hei
-def six_16():
-	global k, flag_x, wid, hei, wid_plus, rel_hei_coils, flag_relx, flag_rely, btn_pady, timer_relx, size, width, width_pic,rel_wid,hei_plus,rel_y
-	k = 40
-	flag_x = 36
-	wid = 600
-	hei = 675
+	rel_y = 125/hei
+"""k = 40
+	flag_x = 47
+	wid = 750
+	hei = 875
 	wid_plus = 800
-	rel_hei_coils = 1
+	rel_hei = wid / hei
 	flag_relx = 0.15
 	flag_rely = 0.18
 	btn_pady = 30
 	timer_relx = 0.85
 	size = 35
 	width = 63
-	width_pic = 63
+	width_pic = 63"""
+def six_16():
+	global n,m,k, flag_x, wid, hei, wid_plus, rel_hei_coils, flag_relx, flag_rely, btn_pady, timer_relx, size,rel_wid,hei_plus,rel_y
+	n = m = 16
+	k = 40
+	flag_x = 30
+	wid = 525
+	hei = 628
+	wid_plus = 800
+	rel_y = 125/hei
+	rel_hei_coils = 0.8
+	flag_relx = 0.15
+	flag_rely = 0.18
+	btn_pady = 30
+	timer_relx = 0.85
+	size = 35
 	rel_wid = 1
 	hei_plus = 50
-	rel_y = 125 / hei
+	
 def eight_8():
-	global k, flag_x, wid, hei, wid_plus, rel_hei_coils, flag_relx, flag_rely, btn_pady, timer_relx, size, width, width_pic,rel_wid,hei_plus,rel_y
+	global n,m,k, flag_x, wid, hei, wid_plus, rel_hei_coils, flag_relx, flag_rely, btn_pady, timer_relx, size,rel_wid,hei_plus,rel_y
+	n = m = 8
 	k = 10
 	flag_x = 63
 	wid = 500
 	hei = 625
 	wid_plus = 800
-	rel_hei_coils = wid / hei
+	rel_hei_coils = wid/hei
 	flag_relx = 0.149
 	flag_rely = 0.28
 	btn_pady = 30
 	timer_relx = 0.851
 	size = 35
-	width = 63
-	width_pic = 63
 	size_records = 20
+	rel_y = 125/hei
+
+def save_flag_recursion():
+	global data
+	data["recursion_akkord"] = enabled.get()
+	with open('data_minesweeper.json', 'w') as file:
+	 	dump(data,file)
+
+def get_records():
+	global record8,record16,record30
+	with open('data_minesweeper.json', 'r') as file:
+		data = load(file)
+		record8 = data["record_8*8"]
+		record16 = data["record_16*16"]
+		record30 = data["record_16*30"]
+		if data["record_8*8"] == 10000:
+			record8 = None
+		if data["record_16*16"] == 10000:
+			record16 = None
+		if data["record_16*30"] == 10000:
+			record30 = None
 
 sizes_of_fields_func = {"8x8": eight_8,"16x16": six_16,"16x30":th_16}
-if m == 30:
-	th_16()
-elif n == m == 16:
-	six_16()
+
+root_smile_face = Tk()
+root_smile_face.title('minesweeper')
+root_smile_face.geometry("500x620+800+50")
+root_smile_face['bg'] = 'gray60'
+default_size = StringVar(value=sizes[0])
+enabled = IntVar(value=data["recursion_akkord"])
+
+def open_main_root():
+	sizes_of_fields_func[default_size.get()]()
+	root_smile_face.destroy()
+
+if os.path.exists('pictures'):
+	os.chdir('pictures')
+	try:
+		pic_good_smile = Image.open('good_smile.png')
+		pic_good_smile = pic_good_smile.resize((63, 63), Image.LANCZOS)
+		pic_good_smile = ImageTk.PhotoImage(pic_good_smile)
+	except:
+		showerror('FileError', 'Required files were not found.')
+		exit()
+	os.chdir('..')
 else:
-	eight_8()
-	
-rel_wid = 1
-hei_plus = 50
-rel_y = 125 / hei
+	showerror('FileError', 'Required files were not found.')
+	exit()
+
+get_records()
+btn_smile_face = Label(root_smile_face, image=pic_good_smile, border=0, activebackground='gray60', background='gray60')
+btn_smile_face.pack(pady=30)
+combobox_sizes_game = ttk.Combobox(root_smile_face, values=sizes, textvariable=default_size, background='gray10',state="readonly")
+combobox_sizes_game.pack()
+btn_play = Button(root_smile_face,text="PLAY",font=('unispace',10),width=20,height=1,background='gray80',activebackground='gray70',border=1,command=open_main_root)
+btn_play.pack(pady=5)
+label_record_88 = Label(root_smile_face,text=f'record 8x8: {record8} sec',background='gray60',font=("unispace",15),width=20,height=2)
+label_record_88.pack()
+label_record_1616 = Label(root_smile_face, text=f'record 16x16: {record16} sec', background='gray60',font=("unispace", 15), width=30, height=2)
+label_record_1616.pack()
+label_record_1630 = Label(root_smile_face, text=f'record 16x30: {record30} sec', background='gray60',font=("unispace", 15), width=30, height=2)
+label_record_1630.pack()
+checkbtn_recursion = Checkbutton(root_smile_face,text='recursion akkord',font=("unispace", 15),width=30, command=save_flag_recursion,height=2,bg='gray60',activebackground='gray60',variable=enabled)
+checkbtn_recursion.pack()
+root_smile_face.mainloop()
+
 end_flag = False
 timer_flag = True
 win_flag = False
+flag_counter = k
+width = width_pic = 63
+count_steps = 0
 
 root = Tk()
 root.title('minesweeper')
 root.geometry(f'{wid}x{hei}+{wid_plus}+{hei_plus}')
-root['bg'] = 'black'
+root['bg'] = 'gray60'
 root.resizable(False,False)
-sizes = ['8x8','16x16','16x30']
 
-sizes = ['8x8','16x16','16x30']
-default_size = StringVar(value=sizes[0])
 enabled = IntVar(value=data["recursion_akkord"])
+
+if os.path.exists('pictures'):
+	flag_y = flag_x
+	os.chdir('pictures')
+	try:
+		pic_good_smile = Image.open('good_smile.png')
+		pic_good_smile = pic_good_smile.resize((width_pic,width_pic), Image.LANCZOS)
+		pic_good_smile = ImageTk.PhotoImage(pic_good_smile)
+
+		pic_cry_smile = Image.open('cry_smile.png')
+		pic_cry_smile = pic_cry_smile.resize((width_pic, width_pic), Image.LANCZOS)
+		pic_cry_smile = ImageTk.PhotoImage(pic_cry_smile)
+
+		win_smile = Image.open('win_smile.png')
+		win_smile = win_smile.resize((width_pic,width_pic), Image.LANCZOS)
+		win_smile = ImageTk.PhotoImage(win_smile)
+
+		pic_flag = Image.open('cell_flag.png')
+		pic_flag = pic_flag.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_flag = ImageTk.PhotoImage(pic_flag)
+
+		picdefault = Image.open('cell.png')
+		pic_default = picdefault.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_default = ImageTk.PhotoImage(pic_default)
+
+		pic_bomb = Image.open('cell_bomb.png')
+		pic_bomb = pic_bomb.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_bomb = ImageTk.PhotoImage(pic_bomb)
+
+		pic_boom_bomb = Image.open('boom_bomb.png')
+		pic_boom_bomb = pic_boom_bomb.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_boom_bomb = ImageTk.PhotoImage(pic_boom_bomb)
+
+		pic_boom_flag = Image.open('boom_flag.png')
+		pic_boom_flag = pic_boom_flag.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_boom_flag = ImageTk.PhotoImage(pic_boom_flag)
+
+		pic_0 = Image.open('cell_0.png')
+		pic_0 = pic_0.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_0 = ImageTk.PhotoImage(pic_0)
+
+		pic_1 = Image.open('cell_1.png')
+		pic_1 = pic_1.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_1 = ImageTk.PhotoImage(pic_1)
+
+		pic_2 = Image.open('cell_2.png')
+		pic_2 = pic_2.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_2 = ImageTk.PhotoImage(pic_2)
+
+		pic_3 = Image.open('cell_3.png')
+		pic_3 = pic_3.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_3 = ImageTk.PhotoImage(pic_3)
+
+		pic_4 = Image.open('cell_4.png')
+		pic_4 = pic_4.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_4 = ImageTk.PhotoImage(pic_4)
+
+		pic_5 = Image.open('cell_5.png')
+		pic_5 = pic_5.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_5 = ImageTk.PhotoImage(pic_5)
+
+		pic_6 = Image.open('cell_6.png')
+		pic_6 = pic_6.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_6 = ImageTk.PhotoImage(pic_6)
+
+		pic_7 = Image.open('cell_7.png')
+		pic_7 = pic_7.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_7 = ImageTk.PhotoImage(pic_7)
+
+		pic_8 = Image.open('cell_8.png')
+		pic_8 = pic_8.resize((flag_x, flag_y), Image.LANCZOS)
+		pic_8 = ImageTk.PhotoImage(pic_8)
+	except:
+		showerror('FileError', 'Required files were not found.')
+		exit()
+	os.chdir('..')
+	list_picks = [pic_0, pic_1, pic_2, pic_3, pic_4, pic_5, pic_6, pic_7, pic_8]
+else:
+	showerror('FileError', 'Required files were not found.')
+	exit()
 
 def bombs():
 	global field_game
@@ -171,7 +316,10 @@ def thread_timer():
 	timer_flag = True
 	while timer_flag:
 		if abs(count_timer-int(count_timer)) <= 0.1:
-			label_timer['text'] = int(count_timer)
+			try:
+				label_timer['text'] = int(count_timer)
+			except:
+				exit()
 		sleep(0.1)
 		count_timer += + 0.1
 	if win_flag:
@@ -184,51 +332,12 @@ def thread_timer():
 			data["record_16*30"] = count_timer
 		with open('data_minesweeper.json','w') as file:
 			dump(data,file)
-			
+
 def open_smile_frame(event):
-
-	def save_flag_recursion():
-		global data
-		data["recursion_akkord"] = enabled.get()
-		with open('data_minesweeper.json', 'w') as file:
-	 		dump(data,file)
-
-	def swap_field_size():
-		sizes_of_fields_func[default_size.get()]()
-		root.resizable(True,True)
-		root.geometry(f'{wid}x{hei}+{wid_plus}+{hei_plus}')
-		root.resizable(False,False)
-		frame_main.place_configure(relwidth=rel_wid, relheight=rel_hei_coils, rely=rel_y)
-
-		pic_default = picdefault.resize((flag_x, flag_y), Image.LANCZOS)
-		pic_default = ImageTk.PhotoImage(pic_default)
-		
-
-
-
-	def forget_smile_face():
-		new_game()
-		frame_smile.place_forget()
-
-	with open('data_minesweeper.json', 'r') as file:
-	 	data = load(file)
-	record8 = data["record_8*8"]
-	record16 = data["record_16*16"]
-	record30 = data["record_16*30"]
-	if data["record_8*8"] == 10000:
-		record8 = None
-	if data["record_16*16"] == 10000:
-		record16 = None
-	if data["record_16*30"] == 10000:
-		record30 = None
-
+	get_records()
 	frame_smile = Frame(root, bg='gray60')
-	btn_smile_face = Button(frame_smile, image=pic_good_smile, border=0, activebackground='gray60', background='gray60',width=width, height=width, command=forget_smile_face)
+	btn_smile_face = Button(frame_smile, image=pic_good_smile, border=0, activebackground='gray60', background='gray60',command=lambda: frame_smile.place_forget(),width=width, height=width)
 	btn_smile_face.pack(pady=btn_pady)
-	combobox_sizes_game = ttk.Combobox(frame_smile, values=sizes, textvariable=default_size, background='gray10',state="readonly")
-	combobox_sizes_game.pack()
-	btn_play = Button(frame_smile,text="PLAY",font=('unispace',10),width=20,height=1,background='gray80',activebackground='gray70',border=1,command=swap_field_size)
-	btn_play.pack(pady=5)
 	label_record_88 = Label(frame_smile,text=f'record 8x8: {record8} sec',background='gray60',font=("unispace",15),width=20,height=2)
 	label_record_88.pack()
 	label_record_1616 = Label(frame_smile, text=f'record 16x16: {record16} sec', background='gray60',font=("unispace", 15), width=30, height=2)
@@ -239,96 +348,19 @@ def open_smile_frame(event):
 	checkbtn_recursion.pack()
 	frame_smile.place(relwidth=1, relheight=1)
 
-flag_counter = k
-flag_y = flag_x
-count_steps = 0
-
-pic_good_smile = Image.open('good_smile.png')
-pic_good_smile = pic_good_smile.resize((width_pic, width_pic), Image.LANCZOS)
-pic_good_smile = ImageTk.PhotoImage(pic_good_smile)
-
-pic_cry_smile = Image.open('cry_smile.png')
-pic_cry_smile = pic_cry_smile.resize((width_pic, width_pic), Image.LANCZOS)
-pic_cry_smile = ImageTk.PhotoImage(pic_cry_smile)
-
-win_smile = Image.open('win_smile.png')
-win_smile = win_smile.resize((flag_x, flag_y), Image.LANCZOS)
-win_smile = ImageTk.PhotoImage(win_smile)
-
 frame_side = Frame(root, bg='gray60')
 label_flags = Label(frame_side, text=flag_counter, font=('unispace', size), background='gray60')
 label_flags.place(relx=flag_relx, rely=flag_rely)
-
 btn_smile = Button(frame_side, image=pic_good_smile, border=0, command=new_game, activebackground='gray60',background='gray60', width=width, height=width)
 btn_smile.bind("<Button-3>", open_smile_frame)
 btn_smile.pack(pady=btn_pady)
-
 label_timer = Label(frame_side, text=0, background='gray60', font=('unispace', size))
 label_timer.place(relx=timer_relx, rely=flag_rely, anchor='ne')
 frame_side.place(relwidth=1, relheight=0.2)
 
-pic_flag = Image.open('cell_flag.png')
-pic_flag = pic_flag.resize((flag_x, flag_y), Image.LANCZOS)
-pic_flag = ImageTk.PhotoImage(pic_flag)
-
-picdefault = Image.open('cell.png')
-pic_default = picdefault.resize((flag_x, flag_y), Image.LANCZOS)
-pic_default = ImageTk.PhotoImage(pic_default)
-
-pic_bomb = Image.open('cell_bomb.png')
-pic_bomb = pic_bomb.resize((flag_x, flag_y), Image.LANCZOS)
-pic_bomb = ImageTk.PhotoImage(pic_bomb)
-
-pic_boom_bomb = Image.open('boom_bomb.png')
-pic_boom_bomb = pic_boom_bomb.resize((flag_x, flag_y), Image.LANCZOS)
-pic_boom_bomb = ImageTk.PhotoImage(pic_boom_bomb)
-
-pic_boom_flag = Image.open('boom_flag.png')
-pic_boom_flag = pic_boom_flag.resize((flag_x, flag_y), Image.LANCZOS)
-pic_boom_flag = ImageTk.PhotoImage(pic_boom_flag)
-
-pic_0 = Image.open('cell_0.png')
-pic_0 = pic_0.resize((flag_x, flag_y), Image.LANCZOS)
-pic_0 = ImageTk.PhotoImage(pic_0)
-
-pic_1 = Image.open('cell_1.png')
-pic_1 = pic_1.resize((flag_x, flag_y), Image.LANCZOS)
-pic_1 = ImageTk.PhotoImage(pic_1)
-
-pic_2 = Image.open('cell_2.png')
-pic_2 = pic_2.resize((flag_x, flag_y), Image.LANCZOS)
-pic_2 = ImageTk.PhotoImage(pic_2)
-
-pic_3 = Image.open('cell_3.png')
-pic_3 = pic_3.resize((flag_x, flag_y), Image.LANCZOS)
-pic_3 = ImageTk.PhotoImage(pic_3)
-
-pic_4 = Image.open('cell_4.png')
-pic_4 = pic_4.resize((flag_x, flag_y), Image.LANCZOS)
-pic_4 = ImageTk.PhotoImage(pic_4)
-
-pic_5 = Image.open('cell_5.png')
-pic_5 = pic_5.resize((flag_x, flag_y), Image.LANCZOS)
-pic_5 = ImageTk.PhotoImage(pic_5)
-
-pic_6 = Image.open('cell_6.png')
-pic_6 = pic_6.resize((flag_x, flag_y), Image.LANCZOS)
-pic_6 = ImageTk.PhotoImage(pic_6)
-
-pic_7 = Image.open('cell_7.png')
-pic_7 = pic_7.resize((flag_x, flag_y), Image.LANCZOS)
-pic_7 = ImageTk.PhotoImage(pic_7)
-
-pic_8 = Image.open('cell_8.png')
-pic_8 = pic_8.resize((flag_x, flag_y), Image.LANCZOS)
-pic_8 = ImageTk.PhotoImage(pic_8)
-
-list_picks = [pic_0, pic_1, pic_2, pic_3, pic_4, pic_5, pic_6, pic_7, pic_8]
-
 field_game = [[0] * m for _ in range(n)]  # field of game -1 = bomb 0-8 = cell
 field_player = [[-2] * m for i in range(n)]  # -2 = closed; -1 = flag; 0-8 = cell;
 field_btns = [[0] * m for i in range(n)]  # list of buttons
-os.chdir('..')
 
 def win():
 	count_closed = 0
@@ -338,7 +370,6 @@ def win():
 				count_closed += 1
 	if count_closed == k:
 		show_field(-1, -1, win=True)
-
 
 def akkord(x, y):
 	recursion = enabled.get()
@@ -373,7 +404,6 @@ def dfs(x, y):
 			if x2 != x or y2 != y:
 				dfs(x2, y2)
 
-
 def show_field(x, y, win=False):
 	global field_btns, btn_smile, end_flag, flag_counter,win_flag,timer_flag
 	timer_flag = False
@@ -398,7 +428,6 @@ def show_field(x, y, win=False):
 				field_btns[i][j]['image'] = pic_bomb
 	btn_smile['image'] = pic_cry_smile
 	field_btns[x][y]['image'] = pic_boom_bomb
-
 
 def action(event):
 	global count_steps, flag_counter, field_game,end_flag
